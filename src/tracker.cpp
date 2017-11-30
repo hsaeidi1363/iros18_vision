f57cac807eac46cc598ed2b3d64fd45278e5b8e9
Hamed Saeidi <hamed.saeidi.rme@gmail.com>
2017-11-30 18:03:19 -0500

    contour and filters working

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -7,6 +7,8 @@
 #include<geometry_msgs/Twist.h>
 #include "planner.h"
 #include "Eigen/Dense"
+#include<trajectory_msgs/JointTrajectory.h> 
+#include<trajectory_msgs/JointTrajectoryPoint.h> 
 
 
 
@@ -15,78 +17,242 @@ using namespace std;
 using namespace cv_bridge;
 
 
-
+// raw input image
 Mat img;
+// pointer for reading ros images via cv brdige
 CvImagePtr cv_ptr;
 
-int maxCorners = 4;
+// maximum number of corners to be detected for the camera calibration using the reference points
 
+
+// canny edge detection threshold
+int thresh = 100;
+
+
+// for producing random colors in the detected features
 RNG rng(12345);
 
+// initialization check to see if the first image is received or not
 bool initialized = false;
 
+// max x and y coordinates take from the camera
 double width, height;
 
-vector<Point2f> refs;
+// references calibration points for the camera (the points on the corners of input image which will be compared against the corners of gauze) 
+vector<Point2f> image_corners;
 	
+// callback function for the input image
 void get_img(const sensor_msgs::Image &  _data){
+	// read the rectified rgb input image from the camera
 	cv_ptr = toCvCopy(_data,"rgb8");
+	// take the image part and put it in a mat variable in opencv format
 	img = cv_ptr->image;
+	// get the width and height
 	width = _data.width;
 	height = _data.height;
+	
 	if (!initialized){
-		refs.push_back(Point2f(0.0f, height));
-		refs.push_back(Point2f(width, height));
-		refs.push_back(Point2f(0.0f,0.0f));
-		refs.push_back(Point2f(width, 0.0f));
+		image_corners.push_back(Point2f(0.0f, height));
+		image_corners.push_back(Point2f(width, height));
+		image_corners.push_back(Point2f(0.0f,0.0f));
+		image_corners.push_back(Point2f(width, 0.0f));
 	}
 	initialized = true;
 }
 
+// find the distance between to XY points
 double distance (double x1, double y1, double x2, double y2){
 	double dx = x1-x2;
 	double dy = y1-y2;
 	return (sqrt(dx*dx+dy*dy));
 }
 
-
 int main(int argc, char * argv[]){
 	ros::init(argc,argv,"tracker");
 	ros::NodeHandle nh_;
+	ros::NodeHandle home("~");
+	
+	bool circle_detection = false;
+	bool contour_detection = false;
+	// number of waypoints sent to the robot
+	int npt = 1;
+
+	home.getParam("circle", circle_detection);
+	home.getParam("contour", contour_detection);	
+	home.getParam("number_of_waypoints", npt);	
+
+	// set the loop frequency equal to the camera input
 	int loop_freq = 7;
 	ros::Rate loop_rate(loop_freq);
+
+	// subscribe to the rectified rgb input (needs the calibration file read in the launch file for the camera)
 	ros::Subscriber cam_sub = nh_.subscribe("camera/image_rect_color",10,get_img);
 
+
+	//publisher for checking the images and debugging them
 	ros::Publisher dbg_pub = nh_.advertise<sensor_msgs::Image>("mydbg",1);
+	// publisher for checking some numerical values
 	ros::Publisher dbg2_pub = nh_.advertise<geometry_msgs::Twist>("imgdbg",1);
+	// publisher for sending the point plans to the trajectory generator of the robot
+	ros::Publisher plan_pub = nh_.advertise<trajectory_msgs::JointTrajectory>("/plan",1);
+
+
+
+	// a debugging variable
 	geometry_msgs::Twist x;
+	// the world coordinates of the reference calibration square
 	vector<Point2f> rect;
-	rect.push_back(Point2f(0.0f, 0.f));
-	rect.push_back(Point2f(0.144f, 0.f));
-	rect.push_back(Point2f(0.0f, 0.144f));
-	rect.push_back(Point2f(0.144f, 0.144f));
+	rect.push_back(Point2f(0.1f, -0.5f));
+	rect.push_back(Point2f(0.1f, -0.4f));
+	rect.push_back(Point2f(0.0f, -0.5f));
+	rect.push_back(Point2f(0.0f, -0.4f));
+
+	// variable for filtering the noise in the corner jumps
+	vector<Point2f> prev_corners_sorted;	
+	prev_corners_sorted.push_back(Point2f(0.0f, 0.0f));
+	prev_corners_sorted.push_back(Point2f(0.0f, 0.0f));
+	prev_corners_sorted.push_back(Point2f(0.0f, 0.0f));
+	prev_corners_sorted.push_back(Point2f(0.0f, 0.0f));
 	
+
+	// the Homography transformation set to Identity matrix initially
 	Eigen::MatrixXd H(3,3);
 	H << 1,0,0,0,1,0,0,0,1;
+	// two temp variables for transforming pixel coordinates to world frame coordinates
 	Eigen::VectorXd X(3);
 	Eigen::VectorXd Xp(3);
 
+	// previous waypoints sent to the robot (used for filtering purposes)
+	vector<Point> prev_way_points;
+	// waypoints sent to the robot
+	vector<Point> way_points;
+	// initialize the previous waypoints
+	for (int i = 0; i < npt; i++){
+		Point pt_tmp;
+		pt_tmp.x = 0; pt_tmp.y=0;		
+		prev_way_points.push_back(pt_tmp);
+	}
 
 	while(ros::ok()){
+		// if an image is received from the camera process it 
 		if(initialized){
-			Mat cimg;
+			// variable for grayscale format of the input image
+			Mat gimg;
+			// convert the input image to grayscale
+			cvtColor(img,gimg, CV_RGB2GRAY);
+			// apply gaussian smoothing of the image
+			GaussianBlur(gimg, gimg, Size(5,5),2,2);			
+			// the corners of the reference square on the ground (pixel coordinates)
+
+			// for circle detection algorithm 
+			if (circle_detection){
+				vector<Vec3f> circles;
+				// use the hough detection algorithm to find the circles			
+				HoughCircles(gimg, circles, HOUGH_GRADIENT, 1, gimg.rows/8,200,50,0,0);
+				for( size_t i = 0; i < circles.size();i++){
+					Vec3i c = circles[i];
+					circle(img, Point(c[0], c[1]) , c[2], Scalar(0,255,255), 3, LINE_AA);
+					circle(img, Point(c[0], c[1]), 2, Scalar(0,255,255), 3, LINE_AA);
+					vector< vector<double> > points = get_points(c[0], c[1], c[2], npt);
+					for (int j = 0; j < npt; j++){
+						circle(img, Point(points[j][0], points[j][1]), 5, Scalar(255,0,0), 3, LINE_AA);
+					} 
+				}
+			}
 			
-			
-			cvtColor(img,cimg, CV_RGB2GRAY);
-			GaussianBlur(cimg, cimg, Size(5,5),2,2);			
+			if (contour_detection){
+				Mat canny_output;
+				vector<vector<Point> > contours;
+				// contours inside the region of interest
+				vector<vector<Point> > masked_contours;
+				// variable used for contour detection
+				vector<Vec4i> hierarchy;
+
+				/// Detect edges using canny
+				Canny( gimg, canny_output, thresh, thresh*2, 3 );
+				/// Find contours
+				findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+				/// detect the contours that are close to the center of image
+				for( int i = 0; i< contours.size(); i++ ){
+					bool in_region =true;
+					for (int k = 0; k < contours[i].size(); k++){
+						Point pt = contours[i][k];
+						if (distance(pt.x, pt.y, width/2,height/2) > 200){
+							in_region = false;
+						}
+					}
+					if(in_region){	
+						// collect the contours in the region of interest
+						masked_contours.push_back(contours[i]);			
+						// only draw the first one for now		
+						if(masked_contours.size() == 1){
+							drawContours( img, contours, i, Scalar(255,255,0), 2, 8, hierarchy, 0, Point() );
+							// a variable for changing the color of waypoints from red (first waypoint) to the green (last waypoint)
+							int col_inc = 255/npt;
+							int color_ctr = 0;
+							// calculate the length of the contour in pixels (from the start pixel to the end pixel)
+							int cont_size = masked_contours[0].size();
+							// add the distance between the final and initial point as a starting value (when sweeping the points the all of the points except the last are considered)
+							double cont_len = distance(masked_contours[0][0].x,masked_contours[0][0].y, masked_contours[0][cont_size-1].x,masked_contours[0][cont_size-1].y);
+							// calculate the length of the contour in pixels
+							for (int k = 0; k < cont_size-1; k++){
+								double ds = distance(masked_contours[0][k].x,masked_contours[0][k].y, masked_contours[0][k+1].x,masked_contours[0][k+1].y); 
+								cont_len += ds;
+							}
+						
+							Point pt;
+							double seg_len = 0;
+							double seg_len_prev = 0;
+							way_points.clear();	
+							pt = masked_contours[0][0];
+							// put the first point of the contour in the waypoint list
+							way_points.push_back(pt);
+							// scan through the points and try to produce equally distanced waypoints using the total length of the contour 
+							for (int k = 0; k < cont_size-1; k++){
+								// calculate the length of a segment on the contour
+								seg_len += distance(masked_contours[0][k].x,masked_contours[0][k].y, masked_contours[0][k+1].x,masked_contours[0][k+1].y); 
+								// check if it is close to the average length (total_length/number_of_waypoints)
+								if( (seg_len_prev < cont_len/npt) && (seg_len > cont_len/npt) ){
+									// add this point to the waypoint list
+									pt = masked_contours[0][k];
+									way_points.push_back(pt);				
+									// reset the length			
+									seg_len = 0;
+								}
+								seg_len_prev = seg_len;
+							}	
+							// add the final point of the contour to the waypoint list
+							pt = masked_contours[0][masked_contours[0].size()-1];
+							way_points.push_back(pt);
+							// lowpass filter for the position of the way points, set tau = 0.0 to deactivate the filter
+							double tau = 0.8;
+							// filter the noise in the waypoints (prevent suddent jumps)
+							for (int k = 0; k < way_points.size(); k++){
+								way_points[k].x = (1-tau)*way_points[k].x + tau*prev_way_points[k].x;
+								way_points[k].y = (1-tau)*way_points[k].y + tau*prev_way_points[k].y;
+								// show them on the image
+								circle(img, Point(way_points[k].x, way_points[k].y), 5, Scalar(255-k*col_inc,0+k*col_inc,0), 3, LINE_AA);
+							}
+							// save the last value for the next filtering loop
+							prev_way_points = way_points;
+						}
+					}
+				 }
+			// end of contour detection
+			 }
+
+		//// beginning of the corner detection 			
 			vector<Point2f> corners;
+			// parameters of the corner detection algorithm
+			int maxCorners = 4;
 			double qualityLevel = 0.01;
 			double minDistance = 100;
-			int blockSize = 10;
+			int blockSize = 2;
 			bool useHarrisDetector = false;
 			double k = 0.04;
-
-			goodFeaturesToTrack( cimg,
+			// detect the corners of the square using the smoothed grayscale image
+			goodFeaturesToTrack( gimg,
                corners,
                maxCorners,
                qualityLevel,
@@ -96,35 +262,15 @@ int main(int argc, char * argv[]){
                useHarrisDetector,
                k );
 
-
-
-			vector<Vec3f> circles;
-			HoughCircles(cimg, circles, HOUGH_GRADIENT, 1, cimg.rows/8,200,50,0,0);
-			for( size_t i = 0; i < circles.size();i++){
-				Vec3i c = circles[i];
-				circle(img, Point(c[0], c[1]) , c[2], Scalar(0,255,255), 3, LINE_AA);
-				circle(img, Point(c[0], c[1]), 2, Scalar(0,255,255), 3, LINE_AA);
-				int n_points = 1;
-				vector< vector<double> > points = get_points(c[0], c[1], c[2], n_points);
-				for (int j = 0; j < n_points; ++j){
-					circle(img, Point(points[j][0], points[j][1]), 5, Scalar(255,0,0), 3, LINE_AA);
-				} 
-				Xp(0) = points[0][0]; Xp(1) = points[0][1]; Xp(2) = 1;
-			}
-			
-			int r = 6;
+			// sort the corners using the image corners to match order of the rect corners
 			vector<double> sort_tmp;
-			for( int i = 0; i < corners.size(); i++ )
-			{ 
-				circle( img, corners[i], r, Scalar(255,0,0), -1, 8, 0 );
-			}
-
 			vector<Point2f> corners_sorted;			
-			for (int i = 0 ; i < refs.size() ; i++){
+			// find the point closest to each corner of the image 
+			for (int i = 0 ; i < image_corners.size() ; i++){
 				double min_dist = 10000;
 				double min_ind = 0;
 				for (int j = 0; j < corners.size(); j++){
-					double dist = distance(refs[i].x,refs[i].y,corners[j].x,corners[j].y);
+					double dist = distance(image_corners[i].x,image_corners[i].y,corners[j].x,corners[j].y);
 					if (dist < min_dist){
 						min_ind = j;
 						min_dist = dist;
@@ -132,36 +278,88 @@ int main(int argc, char * argv[]){
 				}
 				corners_sorted.push_back(corners[min_ind]);
 			}
-				
+			//filter the position of the corners to prevent suddent jumps
+			double tau = 0.95;
+			for (int i = 0; i < corners_sorted.size() ; i++){
+				corners_sorted[i].x = (1-tau)*corners_sorted[i].x + tau*prev_corners_sorted[i].x;
+				corners_sorted[i].y = (1-tau)*corners_sorted[i].y + tau*prev_corners_sorted[i].y;
+				prev_corners_sorted[i].x = corners_sorted[i].x;
+				prev_corners_sorted[i].y = corners_sorted[i].y;
+			}
+			// show the corners
+			for( int i = 0; i < corners_sorted.size(); i++ ){ 
+				circle( img, corners_sorted[i], 6, Scalar(255,0,0), -1, 8, 0 );
+			}
+
+			// find the homography transformation using the world frame coordinates for rect and their associate pixel frame coordinates
 			Mat Hh = findHomography( rect, corners_sorted, CV_RANSAC );
 
-
-			cv_ptr->image = img;
-			dbg_pub.publish(cv_ptr->toImageMsg());	
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 3; j++)
+					H(i,j) = Hh.at<double>(i,j);
 			
 
-			H(0,0) = Hh.at<double>(0,0);
-			H(0,1) = Hh.at<double>(0,1);
-			H(0,2) = Hh.at<double>(0,2);
-			H(1,0) = Hh.at<double>(1,0);
-			H(1,1) = Hh.at<double>(1,1);
-			H(1,2) = Hh.at<double>(1,2);
-			H(2,0) = Hh.at<double>(2,0);
-			H(2,1) = Hh.at<double>(2,1);
-			H(2,2) = Hh.at<double>(2,2);
+/////
+			//H(0,0) = 0.187539;
+			//H(0,1) = 0.982225;
+			//H(0,2) = 596.3800;
+			//H(1,0) = 0.9822533;
+			//H(1,1) = -0.1875547;
+			//H(1,2) = -53.55759;
+			//H(2,0) = 0.0026587;
+			//H(2,1) = 0.0074549;
+			//H(2,2) = -399.50405;
+			
+
+/////
 
 
+			trajectory_msgs::JointTrajectory plan;
+			// debugging 
+			/*for (int k = 0; k <4; k++){
+				trajectory_msgs::JointTrajectoryPoint plan_pt;
+				Xp(0) = corners_sorted[k].x;
+				Xp(1) = corners_sorted[k].y;
+				Xp(2) = 1;
+				X = H.inverse()*Xp;
+				plan_pt.positions.push_back(X(0));
+				plan_pt.positions.push_back(X(1));
+				
+				plan_pt.positions.push_back(0.6143);
+				plan_pt.positions.push_back(3.1);
+				plan_pt.positions.push_back(0.002);
+				plan_pt.positions.push_back(1.8415);
+				for (int ind= 0; ind < 6; ind++){
+					plan_pt.velocities.push_back(0);
+					plan_pt.accelerations.push_back(0);
+				}
+				plan.points.push_back(plan_pt);
+			
+			}*/
+	
+			// xyzrpy waypoints		
+			for (int k = 0; k < way_points.size(); k++){
+				Xp(0) = way_points[k].x; Xp(1) = way_points[k].y; Xp(2) = 1;
+				X = H.inverse()*Xp;
+				trajectory_msgs::JointTrajectoryPoint plan_pt;
+				plan_pt.positions.push_back(X(0));
+				plan_pt.positions.push_back(X(1));
+				plan_pt.positions.push_back(0.6143);
+				plan_pt.positions.push_back(3.1);
+				plan_pt.positions.push_back(0.002);
+				plan_pt.positions.push_back(1.8415);
+				for (int ind= 0; ind < 6; ind++){
+					plan_pt.velocities.push_back(0);
+					plan_pt.accelerations.push_back(0);
+				}
+				plan.points.push_back(plan_pt);
+			}
 
-			X = H.inverse()*Xp;
-			x.linear.x= Xp(0);
-			x.linear.y= Xp(1);
-			x.linear.z= Xp(2);
-			x.angular.x= X(0);
-			x.angular.y= X(1);
-			x.angular.z= X(2);
-
-
-			dbg2_pub.publish(x);
+			plan_pub.publish(plan);
+			// show the image with detected points
+			cv_ptr->image = img;
+			dbg_pub.publish(cv_ptr->toImageMsg());
+			
 		}		
 		ros::spinOnce();
 		loop_rate.sleep();
